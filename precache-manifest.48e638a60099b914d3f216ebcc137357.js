ab9410e508febb2e30526b2d8b5de8416b29cb1a
GAURAV_RAJPUT <gaurav1993rajput@gmai.com>
2020-05-30 18:06:19 +0530

    Updates

---
+++ precache-manifest.48e638a60099b914d3f216ebcc137357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c453a57c42aef0c714c5ceee195aa5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "56236731d032ab5788da",
+    "url": "/weather-app/static/css/main.9e84e4c3.chunk.css"
+  },
+  {
+    "revision": "6f9da207b79e184da8ff",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56236731d032ab5788da",
+    "url": "/weather-app/static/js/main.a8771f98.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "faf1b574cc4e564fa175e4f322e83b3c",
+    "url": "/weather-app/static/media/warm.faf1b574.jpg"
+  }
+]);
